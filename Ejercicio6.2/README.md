74aea32305f71f345f82db727f739098488bd872
paulatiti <paula.boerner@estudiantat.upc.edu>
2024-10-20 21:41:46 +0200

    Add files via upload

---
+++ Ejercicio6.2/README.md
@@ -0,0 +1,1 @@
+# Ejercicio Guia 2 Paula  
---
+++ Ejercicio6.2/clienteEjercicioGuia/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        Thread atender;
+
+ 
+
+        public Form1()
+        {
+            InitializeComponent();
+            //CheckForIllegalCrossThreadCalls = false; //Necesario para que los elementos de los formularios puedan ser
+            //accedidos desde threads diferentes a los que los crearon
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+
+
+        private void PonContador(string mensaje)
+        {
+            contLbl.Text = mensaje;
+        }
+
+
+
+
+        private void AtenderServidor()
+        {
+            while (true)
+            {
+                //Recibimos mensaje del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                string[] trozos = Encoding.ASCII.GetString(msg2).Split('/');
+                int codigo = Convert.ToInt32(trozos[0]);
+                string mensaje = mensaje = trozos[1].Split('\0')[0];
+
+                switch (codigo)
+                {
+                    case 1:  // respuesta a longitud
+
+                        MessageBox.Show("La longitud de tu nombre es: " + mensaje);
+                        break;
+                    case 2:      //respuesta a si mi nombre es bonito
+
+                        if (mensaje == "SI")
+                            MessageBox.Show("Tu nombre ES bonito.");
+                        else
+                            MessageBox.Show("Tu nombre NO bonito. Lo siento.");
+                        break;
+                    case 3:       //Recibimos la respuesta de si soy alto
+
+                        MessageBox.Show(mensaje);
+                        break;
+                    case 4:     //Recibimos notificacion
+
+                        //Haz tu lo que no me dejas hacer a mi
+                        contLbl.Invoke(new Action(() =>
+                        {
+                            contLbl.Text = mensaje;
+                        }));
+
+                        break;
+                }
+            }
+        }
+
+
+                    
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
+            //al que deseamos conectarnos
+            IPAddress direc = IPAddress.Parse("192.168.56.102");
+            IPEndPoint ipep = new IPEndPoint(direc, 9050);
+            
+
+            //Creamos el socket 
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);//Intentamos conectar el socket
+                this.BackColor = Color.Green;
+                MessageBox.Show("Conectado");
+                //pongo en marcha el thread que atenderá los mensajes del servidor
+                ThreadStart ts = delegate { AtenderServidor(); };
+                atender = new Thread(ts);
+                atender.Start();
+
+            }
+            catch (SocketException ex)
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            }
+
+        
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Longitud.Checked)
+            {
+                string mensaje = "1/" + nombre.Text;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+            }
+            else if (Bonito.Checked)
+            {
+                string mensaje = "2/" + nombre.Text;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+             
+
+            }
+            else
+            {
+                // Enviamos nombre y altura
+                string mensaje = "3/" + nombre.Text + "/" + alturaBox.Text;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+              
+            }
+             
+        
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Mensaje de desconexión
+            string mensaje = "0/";
+        
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            // Nos desconectamos
+            atender.Abort();
+            this.BackColor = Color.Gray;
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+
+
+        }
+
+     
+     
+    }
+}
